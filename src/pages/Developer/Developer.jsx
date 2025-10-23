e05c2add8caec74d08fbc90c09383d5ecdc6e626
am-mahmud <am.mahmudasif@gmail.com>
2025-10-22 20:52:15 -0400

    New pages added

--- src/pages/Developer/Developer.jsx
+++ src/pages/Developer/Developer.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { RiUpload2Fill } from "react-icons/ri";
 
 const Developer = () => {
     return (
         <div>
-            <h1>This is for developer</h1>
+            <h1 className='text-3xl font-bold space-mono text-center mt-2'>Retro Game Developer?</h1>
+            <div className='border-4 mt-4 mb-6 border-black min-h-screen flex items-center justify-center text-xl'>
+
+    <button
+        type="submit"
+        className="py-4 px-6 text-base md:text-xl font-bold rounded-md cursor-pointer
+                   bg-[#FFC107] border-[#FFD54F] border-2 text-[#444]
+                   transition duration-150 hover:opacity-80 flex items-center gap-2 md:gap-4"
+    >
+        <span><RiUpload2Fill /></span>
+        Upload Game
+    </button>
+
+</div>
+
+
+
         </div>
     );
 };
