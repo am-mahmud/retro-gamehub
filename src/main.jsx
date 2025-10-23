81a41602fe29957c3729751539b3923609f4fe23
am-mahmud <am.mahmudasif@gmail.com>
2025-10-22 22:57:53 -0400

    Main file upated

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from "react-router/dom";
-import router from './routes/Routes';
+// import router from './routes/Routes';
+
 import { ToastContainer } from "react-toastify";
 import AuthProvider from './contexts/AuthProvider/AuthProvider';
+import router from './routes/routes';
 // import { Helmet } from 'react-helmet';
 // import router from './routes/routes';
 
